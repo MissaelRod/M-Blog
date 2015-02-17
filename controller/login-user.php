a60d9223188ea188397cd70a482314bb72dd63d2
MissaelRodriguez <Blackphantom@gmail.com>
2015-02-17 14:03:20 -0800

    no message

--- controller/login-user.php
+++ controller/login-user.php
@@ -4,4 +4,19 @@ require_once(__DIR__ . "/../model/config.php");
 $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
 $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
 
-$query = $_SESSION["connection"]->query("SELECT salt, password FROM users WHERE username = '$useername'");
+$query = $_SESSION["connection"]->query("SELECT salt, password FROM users WHERE username = '$username'");
+
+if($query->num_rows == 1){
+    $row = $query->fetch_array();
+    
+    if($row["password"] === crypt($password, $row["salt"])){
+        $_SESSION["authenticated"] = true;
+      echo "<p>Login successful!</p>";  
+    }
+    else{
+        echo "<p>Invalid username and password1<p>";
+    }
+}
+    else{
+        echo "<p>Invalid username and password<p>";
+    }
--- view/login-form.php
+++ view/login-form.php
@@ -4,7 +4,7 @@ require_once (__DIR__ . "/../model/config.php");
 
 <h1>Login</h1>
 
-<form method="post" action="<?php echo $path . "controller/login-user.php"; ?> >
+<form method="post" action="<?php echo $path . "controller/login-user.php"; ?>">
      <div>
         <lable for="username">Username: </lable>
         <input type="text" name="username" />
